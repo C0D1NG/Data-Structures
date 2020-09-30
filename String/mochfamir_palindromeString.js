561206c4993def01c67e55c51017c819396ab632
givemo <gtchiwanza@gmail.com>
2020-09-30 14:36:57 +0100

    add palindrome string check

--- String/mochfamir_palindromeString.js
+++ String/mochfamir_palindromeString.js
@@ -1,11 +1,11 @@
 function checkPalindrome(word) {
- for (let i = 0; i < Math.floor(word.length / 2); i++) {
-  if (word[i] !== word[word.length - i - 1]) {
-   return false;
+  for (let i = 0; i < Math.floor(word.length / 2); i++) {
+    if (word[i] !== word[word.length - i - 1]) {
+      return false;
+    }
   }
- }
- return true;
+  return true;
 }
 
-console.log(checkPalindrome('hello'));
-console.log(checkPalindrome('racecar'));
+console.log(checkPalindrome("hello"));
+console.log(checkPalindrome("racecar"));
