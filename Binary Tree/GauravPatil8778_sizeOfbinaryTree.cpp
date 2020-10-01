9041aa139b0ec3199fbd0bd4a5a5dcec8492f2a2
GAURAV PATIL <67456816+GauravPatil8778@users.noreply.github.com>
2020-10-01 19:07:59 +0530

    Create GauravPatil8778_sizeOfbinaryTree.cpp

---
+++ Binary Tree/GauravPatil8778_sizeOfbinaryTree.cpp
@@ -0,0 +1,48 @@
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+/* A binary tree node has data, pointer to left child 
+and a pointer to right child */
+class node 
+{ 
+	public: 
+	int data; 
+	node* left; 
+	node* right; 
+}; 
+
+/* Helper function that allocates a new node with the 
+given data and NULL left and right pointers. */
+node* newNode(int data) 
+{ 
+	node* Node = new node(); 
+	Node->data = data; 
+	Node->left = NULL; 
+	Node->right = NULL; 
+		
+	return(Node); 
+} 
+
+/* Computes the number of nodes in a tree. */
+int size(node* node) 
+{ 
+	if (node == NULL) 
+		return 0; 
+	else
+		return(size(node->left) + 1 + size(node->right)); 
+} 
+
+int main() 
+{ 
+	node *root = newNode(1); 
+	root->left = newNode(2); 
+	root->right = newNode(3); 
+	root->left->left = newNode(4); 
+	root->left->right = newNode(5); 
+	
+	cout << "Size of the tree is " << size(root); 
+	return 0; 
+} 
+
+
