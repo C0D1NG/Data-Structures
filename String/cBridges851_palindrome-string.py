29ea03d1981d6d2eb8b29060c2d3cee8d08574ef
= <=>
2020-10-02 17:55:46 +0100

    User can input a string

---
+++ String/cBridges851_palindrome-string.py
@@ -0,0 +1,9 @@
+# CB 2020-10-02 Palindrome String Checker
+
+# CB 2020-10-02 Take in a string as input
+originalString = input("Please input a string: ")
+# CB 2020-10-02 Reverse the string, store it
+
+# CB 2020-10-02 If the input string is the same as output, it's a palindrome
+
+# CB 2020-10-02 Else, it's not a palindrome
