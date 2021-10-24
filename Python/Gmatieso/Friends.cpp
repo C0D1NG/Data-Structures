112baced05b4e49b14182a256da9c213b9816c92
Gmatieso <matiesogeoffrey@gmail.com>
2021-10-25 01:10:52 +0300

    code refactoring

--- Python/Gmatieso/Friends.cpp
+++ Python/Gmatieso/Friends.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
-int main(){
+class A
+{
+    int a, b, c;
+    public:
+     inline void get();
+     void put();
 
-   cout<<"Hello  world!"<<endl; 
-   
-    return 0;
+};
+void A :: get() {
+    a = 20 ;
+    b = 40 ;
+    c = a + b ;
+}
+
+void A :: put() {
+    cout<< "addition "<<c<<endl;
+}
+
+int main(){
+   A  d; // create object
+   d.get(); 
+   d.put();
+   return 0;
 }
