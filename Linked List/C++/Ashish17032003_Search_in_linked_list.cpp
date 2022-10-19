b987f8f03b3fdd2e76152def51667f21c2b198c2
Ashish17032003 <70490100+Ashish17032003@users.noreply.github.com>
2022-10-19 20:13:11 +0530

    Create Ashish17032003_Search_in_linked_list.cpp

---
+++ Linked List/C++/Ashish17032003_Search_in_linked_list.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class Node {
+public:
+    int key;
+    Node* next;
+};
+ 
+void push(Node** head_ref, int new_key)
+{
+  
+    Node* new_node = new Node();
+ 
+ 
+    new_node->key = new_key;
+ 
+  
+    new_node->next = (*head_ref);
+ 
+
+    (*head_ref) = new_node;
+}
+ 
+bool search(Node* head, int x)
+{
+    Node* current = head;
+    while (current != NULL) {
+        if (current->key == x)
+            return true;
+        current = current->next;
+    }
+    return false;
+}
+ 
+
+int main()
+{
+
+    Node* head = NULL;
+    int x = 21;
+ 
+    push(&head, 74);
+    push(&head, 32);
+    push(&head, 11);
+    push(&head, 21);
+    push(&head, 14);
+
+    search(head, 21) ? cout << "Yes" : cout << "No";
+    return 0;
+}
